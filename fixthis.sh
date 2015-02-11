5eea7f9ef67e35eb12d91a8170d45f4176be70ff
Demian <thde@users.noreply.github.com>
2015-02-11 13:28:33 +0100

    preserve premissions

--- fixthis.sh
+++ fixthis.sh
@@ -12,7 +12,7 @@ migrate_mail_directory () {
 	FOLDER_NEW="$2"
 
 	for FOLDER in "$(find ./ -maxdepth 1 -type d -name "$FOLDER_OLD*" -printf '%f\n')"; do
-		rsync -rav "$FOLDER" "${FOLDER//$FOLDER_OLD/$FOLDER_NEW}"
+		rsync -rpav "$FOLDER" "${FOLDER//$FOLDER_OLD/$FOLDER_NEW}"
 		if [[ $? -eq 0 ]]; then 
 			rm -rf "$FOLDER"
 		fi
