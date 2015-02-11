e9f09fdb9a279d2f524fcd30b86b9a3f303777b9
thde <d@thde.io>
2015-02-11 11:44:49 +0100

    added removal of old folders if rsync succeeds

--- fixthis.sh
+++ fixthis.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 migrate_folder () {
-
 	FOLDER_OLD="$1"
 	FOLDER_NEW="$2"
 
-
 	for FOLDER in "$(find ./ -maxdepth 1 -type d -name "$FOLDER_OLD*" -printf '%f\n')"; do
-		FOLDER_TARGET="${FOLDER//$FOLDER_OLD/$FOLDER_NEW}"
-		rsync -rav "$FOLDER" "$FOLDER_TARGET"
+		rsync -rav "$FOLDER" "${FOLDER//$FOLDER_OLD/$FOLDER_NEW}"
+		if [[ $? -eq 0 ]]; then 
+			rm -rf "$FOLDER"
+		fi
 	done
 }
 
