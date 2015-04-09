3c1eb0dbdb73cb53ee9c7130e562d2be42dba71b
Demian <thde@users.noreply.github.com>
2015-04-09 09:58:34 +0200

    added folders

--- fixthis.sh
+++ fixthis.sh
@@ -35,6 +35,8 @@ check_working_directory
 
 migrate_mail_directory ".Gesendete Objekte" ".Sent"
 migrate_mail_directory ".Gesendete Elemente" ".Sent"
+migrate_mail_directory ".Sent Messages" ".Sent"
 migrate_mail_directory ".Entw&APw-rfe" ".Drafts"
 migrate_mail_directory ".Papierkorb" ".Trash"
+migrate_mail_directory ".Deleted Messages" ".Trash"
 migrate_mail_directory ".Spam" ".Junk"
